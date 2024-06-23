6ca369340ccac6788e30cc9590238e83d2964474
MyOneGod <MyOneGod@proton.me>
2024-06-23 16:21:49 +0200

    initial commit

---
+++ metadata.lua
@@ -0,0 +1,10 @@
+return PlaceObj('ModDef', {
+	'title', "[MOG] Impish Equipment",
+	'id', "ir5ndGr",
+	'author', "MyOneGod",
+	'version', 1,
+	'lua_revision', 233360,
+	'saved_with_revision', 350233,
+	'default_options', {},
+	'saved', 1719150676,
+})
